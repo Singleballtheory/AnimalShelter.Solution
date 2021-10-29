6028a504490da52ca523cd7ed49e0a577a5c2148
Scott Hutley <scotthutley1@comcast.net>
2021-10-29 14:05:17 -0700

    Add CreaturesController with full CRUD

---
+++ AnimalShelter/Controllers/CreaturesController.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CreaturesController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+
+    public CreaturesController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Creature>>> Get()
+    {
+      return await _db.Creatures.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Creature>> GetCreature(int id)
+    {
+      var creature = await _db.Creatures.FindAsync(id);
+
+      if (creature == null)
+      {
+        return NotFound();
+      }
+
+      return creature;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<Creature>> Post(Creature creature)
+    {
+      _db.Creatures.Add(creature);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetCreature), new { id = creature.CreatureId }, creature);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Creature creature)
+    {
+      if (id != creature.CreatureId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(creature).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!CreatureExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCreature(int id)
+    {
+      var creature = await _db.Creatures.FindAsync(id);
+      if (creature == null)
+      {
+        return NotFound();
+      }
+
+      _db.Creatures.Remove(creature);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool CreatureExists(int id)
+    {
+      return _db.Creatures.Any(e => e.CreatureId == id);
+    }
+  }
+}
