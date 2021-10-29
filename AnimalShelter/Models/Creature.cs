accfd26df7170cd94d0426b7fad556086d342351
Scott Hutley <scotthutley1@comcast.net>
2021-10-29 10:12:22 -0700

    Add Creature.cs and AnimalShelterContext.cs

---
+++ AnimalShelter/Models/Creature.cs
@@ -0,0 +1,13 @@
+namespace AnimalShelter.Models
+{
+  public class Creature
+  {
+    public int CreatureId { get; set; }
+    public string Name { get; set; }
+    public string Spieces { get; set; }
+    public string Mythical { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public int Age { get; set; }
+  }
+}
